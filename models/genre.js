f14defbe149130022b1db4340fe246263d6acc7e
Stevo99 <Sfailla1983@gmail.com>
2019-10-28 14:43:13 -0400

    minor changes to module exports

--- models/genre.js
+++ models/genre.js
@@ -2,39 +2,41 @@ const mongoose = require('mongoose');
 
 // Genre Schema
 const genreSchema = mongoose.Schema({
-  name:{
-      type: String,
-      required: true
-    },
-    create_date:{
-        type: Date,
-        default: Date.now
-    }
+	name: {
+		type: String,
+		required: true
+	},
+	create_date: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-const Genre = module.exports = mongoose.model('Genre', genreSchema);
+const Genre = mongoose.model('Genre', genreSchema);
+
+module.exports = Genre;
 
 // Get genres
 module.exports.getGenres = (callback, limit) => {
-  Genre.find(callback).limit(limit);
-}
+	Genre.find(callback).limit(limit);
+};
 
 // Adding a Genre
 module.exports.addGenre = (genre, callback) => {
-  Genre.create(genre, callback);
-}
+	Genre.create(genre, callback);
+};
 
 // Updating a Genre
 module.exports.updateGenre = (id, genre, options, callback) => {
-  const query = { _id: id };
-  const update = {
-    name: genre.name,
-  }
-  Genre.findOneAndUpdate(query, update, options, callback);
-}
+	const query = { _id: id };
+	const update = {
+		name: genre.name
+	};
+	Genre.findOneAndUpdate(query, update, options, callback);
+};
 
 // Delete a Genre
 module.exports.removeGenre = (id, callback) => {
-  const query = { _id: id };
-  Genre.remove(query, callback);
-}
+	const query = { _id: id };
+	Genre.remove(query, callback);
+};
--- models/movie.js
+++ models/movie.js
@@ -2,66 +2,61 @@ const mongoose = require('mongoose');
 
 // Movie Schema This refers to the info in MongoDB
 const movieSchema = mongoose.Schema({
-    title:{
-      type: String,
-      required: true,
-      unique: true
-    },
-    genre: {
-      type: String,
-      required: true
-    },
-    description: {
-      type: String
-    },
-    director: {
-      type: String
-    },
-    starring: {
-      type: String
-    },
-    img_url: {
-      type: String
-    },
-    create_date:{
-        type: Date,
-        default: Date.now
-    }
+	title: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	genre: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String
+	},
+	director: {
+		type: String
+	},
+	starring: {
+		type: String
+	},
+	img_url: {
+		type: String
+	},
+	create_date: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-const Movie = module.exports = mongoose.model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
 
 // Get Movies
-module.exports.getMovies = (callback, limit) => {
-  Movie.find(callback).limit(limit);
-}
-
-// Get Movie
-module.exports.getMovieById = (id, callback) => {
-  Movie.findById(id, callback);
-}
-
-// Adding a movie
-module.exports.addMovie = (movie, callback) => {
-  Movie.create(movie, callback)
-}
-
-// Updating a movie
-module.exports.updateMovie = (id, movie, options, callback) => {
-  const query = { _id: id }
-  const update = {
-    title: movie.title,
-    genre: movie.genre,
-    discription: movie.description,
-    director: movie.director,
-    starring: movie.starring,
-    img_url: movie.img_url
-  }
-  Movie.findOneAndUpdate(query, update, options, callback);
-}
-
-// Delete a Movie
-module.exports.removeMovie = (id, callback) => {
-  const query = { _id: id };
-  Movie.remove(query, callback);
-}
+module.exports = {
+	getMovies(callback, limit) {
+		Movie.find(callback).limit(limit);
+	},
+	getMovieById(id, callback) {
+		Movie.findById(id, callback);
+	},
+	addMovie(movie, callback) {
+		Movie.create(movie, callback);
+	},
+	updateMovie(id, movie, options, callback) {
+		const query = { _id: id };
+		const update = {
+			title: movie.title,
+			genre: movie.genre,
+			discription: movie.description,
+			director: movie.director,
+			starring: movie.starring,
+			img_url: movie.img_url
+		};
+		Movie.findOneAndUpdate(query, update, options, callback);
+	},
+	removeMovie(id, callback) {
+		const query = { _id: id };
+		Movie.remove(query, callback);
+	}
+};
