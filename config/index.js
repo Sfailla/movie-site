3b343a610a54515ca85f30556359f665763d75cf
Stevo99 <Sfailla1983@gmail.com>
2019-10-28 14:45:28 -0400

    moved all mongoose connection to client.js file

--- config/index.js
+++ config/index.js
@@ -1,7 +1,21 @@
-var configVal = require('./config');
+const mongoose = require('mongoose');
+var config = require('./config.json');
+
+function connectToMongoose() {
+	const getDBConnection = `mongodb://${config.username}:${config.password}@ds127044.mlab.com:27044/moviesite`;
+	mongoose.Promise = global.Promise;
+
+	function makeDBConnection() {
+		mongoose
+			.connect(getDBConnection, {
+				useMongoClient: true
+			})
+			.then(() => console.log('Mlab is connected...'))
+			.catch(err => console.error(err));
+	}
+	return makeDBConnection();
+}
 
 module.exports = {
-	getDBConnection: function() {
-		return `mongodb://${configVal.username}:${configVal.password}@ds127044.mlab.com:27044/moviesite`;
-	}
+	connectToMongoose
 };
