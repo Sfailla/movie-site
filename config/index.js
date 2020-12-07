1432d2380a3e052310dc7a0bb6e62f87bd231301
Sfailla <Sfailla1983@gmail.com>
2020-12-07 15:04:13 -0500

    removed config file.  added dotenv for environment variables

--- config/index.js
+++ config/index.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 var config = require('./config.json');
 
 function connectToMongoose() {
-	const getDBConnection = `mongodb://${config.username}:${config.password}@ds127044.mlab.com:27044/moviesite`;
 	mongoose.Promise = global.Promise;
 
 	function makeDBConnection() {
 		mongoose
-			.connect(getDBConnection, {
+			.connect(process.env.MONGOOSE_URI, {
 				useMongoClient: true
 			})
 			.then(() => console.log('Mlab is connected...'))
